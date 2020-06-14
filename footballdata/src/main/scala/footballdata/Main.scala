7b18d0e40036acc52d72de19438be18346e52440
James Palmer <james.palmer@itv.com>
2020-06-14 12:45:49 +0100

    Initial commit

---
+++ footballdata/src/main/scala/footballdata/Main.scala
@@ -0,0 +1,9 @@
+package footballdata
+
+import cats.effect.{ExitCode, IO, IOApp}
+import cats.implicits._
+
+object Main extends IOApp {
+  def run(args: List[String]) =
+    FootballdataServer.stream[IO].compile.drain.as(ExitCode.Success)
+}
