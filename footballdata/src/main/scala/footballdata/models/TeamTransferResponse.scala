798fc02a6f0c3cbea19491737b862a1dcf6fa802
James Palmer <james.palmer@itv.com>
2020-06-26 20:12:40 +0100

    Update to my own decodings

--- footballdata/src/main/scala/footballdata/models/TeamTransferResponse.scala
+++ footballdata/src/main/scala/footballdata/models/TeamTransferResponse.scala
@@ -1,7 +1,7 @@
 package footballdata.models
 
 import cats.effect.Sync
-import io.circe.generic.semiauto.{deriveDecoder,  deriveEncoder}
+import io.circe.generic.semiauto.deriveEncoder
 import io.circe.{Decoder, Encoder, HCursor}
 import org.http4s.EntityDecoder
 import org.http4s.circe.jsonOf
@@ -21,78 +21,57 @@ object TeamTransferResponse {
 final case class TransferApi(results: Int, transfers: Seq[TransferData])
 
 object TransferApi{
-  implicit val decoder: Decoder[TransferApi] = deriveDecoder
 
-  //  implicit val decoder: Decoder[TransferApi] = (c: HCursor) =>
-//    for {
-//      results <- c.downField("results").as[Int]
-//      transfers <- c.downField("transfers").as[Seq[TransferData]]
-//    } yield TransferApi(results, transfers)
+    implicit val decoder: Decoder[TransferApi] = (c: HCursor) =>
+    for {
+      results <- c.downField("results").as[Int]
+      transfers <- c.downField("transfers").as[Seq[TransferData]]
+    } yield TransferApi(results, transfers)
 
   implicit val encoder: Encoder[TransferApi] = deriveEncoder
 }
 
 
 final case class TransferData(
-                               player_id: Int,
-                               player_name: String,
-                               transfer_date: String,
-                               `type`: Option[String],
-                               team_in: Team,
-                               team_out: Team,
-                               lastUpdate: Int
+                               playerId: Int,
+                               playerName: String,
+                               transferDate: String,
+                               transferType: Option[String],
+                               teamIn: Team,
+                               teamOut: Team,
                              )
 
-/*
-"player_id": 138827,
-                "player_name": "B. Duncan",
-                "transfer_date": "2019-09-02",
-                "type": "€ 1.8M",
-                "team_in": {
-                    "team_id": 502,
-                    "team_name": "Fiorentina"
-                },
-                "team_out": {
-                    "team_id": 40,
-                    "team_name": "Liverpool"
-                },
-                "lastUpdate": 1577932180
- */
-
 object TransferData {
-  implicit val decoder: Decoder[TransferData] = deriveDecoder
 
-//  implicit val decoder: Decoder[TransferData] = (c: HCursor) =>
-//    for {
-//      playerId     <- c.downField("player_id").as[Int]
-//      playerName   <- c.downField("player_name").as[String]
-//      transferData <- c.downField("transfer_date").as[String]
-//      transferType <- c.downField("type").as[String]
-//      teamIn       <- c.downField("team_in").as[Team]
-//      teamOut      <- c.downField("team_out").as[Team]
-//      lastUpdate   <- c.downField("lastUpdate").as[Int]
-//    } yield TransferData(
-//      playerId,
-//      playerName,
-//      transferData,
-//      transferType,
-//      teamIn,
-//      teamOut,
-//      lastUpdate
-//    )
+  implicit val decoder: Decoder[TransferData] = (c: HCursor) =>
+    for {
+      playerId     <- c.downField("player_id").as[Int]
+      playerName   <- c.downField("player_name").as[String]
+      transferDate <- c.downField("transfer_date").as[String]
+      transferType <- c.downField("type").as[Option[String]]
+      teamIn       <- c.downField("team_in").as[Team]
+      teamOut      <- c.downField("team_out").as[Team]
+      _   <- c.downField("lastUpdate").as[Int]
+    } yield TransferData(
+      playerId,
+      playerName,
+      transferDate,
+      transferType,
+      teamIn,
+      teamOut
+    )
 
   implicit val encoder: Encoder[TransferData] = deriveEncoder
 }
 
-final case class Team(team_id: Option[Int], team_name: Option[String])
+final case class Team(teamId: Option[Int], teamName: Option[String])
 
 object Team {
-  implicit val decoder: Decoder[Team] = deriveDecoder
-//  implicit val decoder: Decoder[Team] = (c: HCursor) =>
-//    for {
-//      teamId   <- c.downField("team_id").as[Int]
-//      teamName <- c.downField("team_name").as[String]
-//  } yield Team(teamId, teamName)
+  implicit val decoder: Decoder[Team] = (c: HCursor) =>
+    for {
+      teamId   <- c.downField("team_id").as[Option[Int]]
+      teamName <- c.downField("team_name").as[Option[String]]
+  } yield Team(teamId, teamName)
 
   implicit val encoder: Encoder[Team] = deriveEncoder
 }
