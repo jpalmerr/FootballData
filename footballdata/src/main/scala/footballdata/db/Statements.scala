bc85669d63e135483c3f87afb4250389d32c0246
James Palmer <james.palmer@itv.com>
2020-06-28 16:25:59 +0100

    Add conflict logic

--- footballdata/src/main/scala/footballdata/db/Statements.scala
+++ footballdata/src/main/scala/footballdata/db/Statements.scala
@@ -7,5 +7,5 @@ import footballdata.models.TransferData
 object Statements {
 
   def upsert(td: TransferData): Update0 =
-    sql"INSERT INTO transactions (teamoutid, teamoutname, teaminid, teaminname, playername, transferdate, transfertype) VALUES (${td.teamOut.teamId}, ${td.teamOut.teamName}, ${td.teamIn.teamId}, ${td.teamIn.teamName}, ${td.playerName}, ${td.transferDate}, ${td.transferType})".update
+    sql"INSERT INTO transactions (teamoutid, teamoutname, teaminid, teaminname, playername, transferdate, transfertype) VALUES (${td.teamOut.teamId}, ${td.teamOut.teamName}, ${td.teamIn.teamId}, ${td.teamIn.teamName}, ${td.playerName}, ${td.transferDate}, ${td.transferType}) ON CONFLICT (teamoutid, teaminid, playername) DO NOTHING".update
 }
